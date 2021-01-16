b85a5a5fa8ae3bf12bbbd0320e7a1302da6407cb
Ashutosh00710 <starcdwivedi@gmail.com>
2021-01-16 19:50:50 +0530

    Code refactoring

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
 import express, { Application, Request, Response } from 'express';
-import { calendarData, queryOptions } from './utils';
-import { Card } from './GraphCards';
+import { getGraph } from './utils';
 import bodyParser from 'body-parser';
-import { invalidUserSvg } from './svgs';
-import { queryOption } from '../interfaces/interface';
-import axios from 'axios';
 
 const app: Application = express();
 let port: string | number = process.env.PORT || 5100;
@@ -16,40 +12,8 @@ app.get('/', (req: Request, res: Response) => {
   res.send(`<h1>GitHub Readme Activity Graph 📈</h1>`);
 });
 
-app.get(
-  '/graph',
-  async (req: Request, res: Response): Promise<void> => {
-    try {
-      const options: queryOption = queryOptions(req.query);
-
-      const fetchCalendarData: string | number[] = await calendarData(
-        `${options.username}`,
-        axios //Dependency Injection
-      );
-
-      if (Array.isArray(fetchCalendarData)) {
-        const graph: Card = new Card(
-          420,
-          800,
-          options.colors,
-          `${options.username}'s Contribution Graph`,
-          options.area
-        );
-
-        const getChart: string = await graph.chart(fetchCalendarData);
-
-        res.setHeader('Cache-Control', 'public, max-age=900');
-        res.set('Content-Type', 'image/svg+xml');
-        res.status(200).send(getChart);
-      } else {
-        res.set('Content-Type', 'image/svg+xml');
-        res.send(invalidUserSvg(fetchCalendarData));
-      }
-    } catch (error) {
-      res.send(invalidUserSvg('Something unexpected happened 💥'));
-    }
-  }
-);
+//Get Graph
+app.get('/graph', getGraph);
 
 app.listen(port, (): void => {
   console.log(`Server is Running in port ${port}`);
--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,11 @@
-import { AxiosResponse, AxiosStatic } from 'axios';
+import { Request, Response } from 'express';
+import { Card } from './GraphCards';
+import { invalidUserSvg } from './svgs';
+import axios, { AxiosResponse, AxiosStatic } from 'axios';
 import { queryOption, colors, resp } from '../interfaces/interface';
 import { selectColors } from '../styles/themes';
 
-export const calendarData = async (
+const calendarData = async (
   userId: string,
   axios: AxiosStatic
 ): Promise<number[] | string> => {
@@ -18,7 +21,7 @@ export const calendarData = async (
   }
 };
 
-export const queryOptions = (queryString: any): queryOption => {
+const queryOptions = (queryString: any): queryOption => {
   let area: boolean = false;
   let colors: colors;
   if (String(queryString.area) === 'true') {
@@ -26,26 +29,22 @@ export const queryOptions = (queryString: any): queryOption => {
   }
 
   if (queryString.theme) {
-    colors = selectColors(String(queryString.theme));
+    colors = selectColors(queryString.theme as string);
   } else {
     //Custom options for user
     colors = {
-      bgColor: String(
-        queryString.bg_color
-          ? queryString.bg_color
-          : selectColors('default').bgColor
-      ),
-      color: String(
-        queryString.color ? queryString.color : selectColors('default').color
-      ),
-      lineColor: String(
-        queryString.line ? queryString.line : selectColors('default').lineColor
-      ),
-      pointColor: String(
-        queryString.point
-          ? queryString.point
-          : selectColors('default').pointColor
-      ),
+      bgColor: queryString.bg_color
+        ? (queryString.bg_color as string)
+        : selectColors('default').bgColor,
+      color: queryString.color
+        ? (queryString.color as string)
+        : selectColors('default').color,
+      lineColor: queryString.line
+        ? (queryString.line as string)
+        : selectColors('default').lineColor,
+      pointColor: queryString.point
+        ? (queryString.point as string)
+        : selectColors('default').pointColor,
     };
   }
 
@@ -56,4 +55,36 @@ export const queryOptions = (queryString: any): queryOption => {
   };
 
   return options;
+};
+
+export const getGraph = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const options: queryOption = queryOptions(req.query);
+
+    const fetchCalendarData: string | number[] = await calendarData(
+      `${options.username}`,
+      axios //Dependency Injection
+    );
+
+    if (Array.isArray(fetchCalendarData)) {
+      const graph: Card = new Card(
+        420,
+        800,
+        options.colors,
+        `${options.username}'s Contribution Graph`,
+        options.area
+      );
+
+      const getChart: string = await graph.chart(fetchCalendarData);
+
+      res.setHeader('Cache-Control', 'public, max-age=900');
+      res.set('Content-Type', 'image/svg+xml');
+      res.status(200).send(getChart);
+    } else {
+      res.set('Content-Type', 'image/svg+xml');
+      res.send(invalidUserSvg(fetchCalendarData));
+    }
+  } catch (error) {
+    res.send(invalidUserSvg('Something unexpected happened 💥'));
+  }
 };
