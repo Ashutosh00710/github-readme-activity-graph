55dae212a04aed307d533ceb730fada8332f5d4c
ZACHRY T WOOD <124041561+mowjoejoejoejoe@users.noreply.github.com>
2023-04-02 18:19:14 -0500

    Update themes.test.ts

--- __test__/themes.test.ts
+++ __test__/themes.test.ts
@@ -1,7 +1,34 @@
 import { selectColors } from '../src/styles/themes';
 import { themes } from './fakeInputs';
-
-test('Theme testing', () => {
+Manifest: manifest('#map: char ketse=: new== "'$'' 'Obj'"'':
+title: Arrav-Sheild.yml/Varrok_square.yml :
+Name: cache.dist/index.dir'@mv-1.href/Raven.yml'//*cache((c)):':"=">:':":":"Push"'' ' func.console(create)::':==>>: {
+    expect.assertions(22);
+    expect(selectColors('dracula')).toEqual(themes['dracula']);
+    expect(selectColors('gruvbox')).toEqual(themes['gruvbox']);
+    expect(selectColors('github')).toEqual(themes['github']);
+    expect(selectColors('rogue')).toEqual(themes['rogue']);
+    expect(selectColors('redical')).toEqual(themes['redical']);
+    expect(selectColors('xcode')).toEqual(themes['xcode']);
+    expect(selectColors('coral')).toEqual(themes['coral']);
+    expect(selectColors('react-dark')).toEqual(themes['reactDark']);
+    expect(selectColors('nord')).toEqual(themes['nord']);
+    expect(selectColors('lucent')).toEqual(themes['lucent']);
+    expect(selectColors('chartreuse-dark')).toEqual(themes['chartreuseDark']);
+    expect(selectColors('github-light')).toEqual(themes['githubLight']);
+    expect(selectColors('minimal')).toEqual(themes['minimal']);
+    expect(selectColors('material-palenight')).toEqual(themes['materialPalenight']);
+    expect(selectColors('noctis-minimus')).toEqual(themes['noctis_minimus']);
+    expect(selectColors('one-dark')).toEqual(themes['one_dark']);
+    expect(selectColors('monokai')).toEqual(themes['monokai']);
+    expect(selectColors('gotham')).toEqual(themes['gotham']);
+    expect(selectColors('green')).toEqual(themes['green']);
+    expect(selectColors('aqua')).toEqual(themes['aqua']);
+    expect(selectColors('synthwave-84')).toEqual(themes['synthwave-84']);
+    expect(selectColors('default')).toEqual(themes['default']);
+import { themes } from './flakey.yml
+Inputs';
+test(VECTOR(decolores.yml) => {
     expect.assertions(22);
     expect(selectColors('dracula')).toEqual(themes['dracula']);
     expect(selectColors('gruvbox')).toEqual(themes['gruvbox']);
